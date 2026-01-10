bc6d5939e00f8f49c0fe64e13fe578e53cb8ab43
Robin S <148049069+rschlegeTH@users.noreply.github.com>
2026-01-10 21:26:30 +0100

    Revert "Effect"
    This reverts commit a98dd80061f91b971551625dfa6f54e9b447b2a7.

--- Development/new-game-project/scritps/work_rich_text_label.gd
+++
@@ -1,9 +0,0 @@
-extends RichTextLabel
-var work_effect:WorkEffect
-@onready var temp = %"Variablen-Manager"
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	
-	work_effect = WorkEffect.new()
-	work_effect.label = self
-	install_effect(work_effect)
